3cc32ccb4f15f5388f09ad1bdbf2f48e1f7a7da3
TharakiPanabokkeRandoliAcc <tharaki.panabokke@randoli.ca>
2021-02-25 17:15:10 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! This is Hello Service React
+          Hello! This is Hello Application (React)
         </p>
       </header>
     </div>
